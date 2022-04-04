3d7eab6829165ec9cb8d09b6fa01700e953241ec
justinpajak <jpajak@nd.edu>
2022-04-03 20:01:56 -0700

    Debug complete.

--- fs.c
+++ fs.c
@@ -33,49 +33,84 @@ union fs_block {
 	char data[DISK_BLOCK_SIZE];
 };
 
-int fs_format()
-{
+int fs_format() {
 	return 0;
 }
 
-void fs_debug()
-{
+void fs_debug() {
+	/* Read superblock data from disk */
 	union fs_block block;
+	disk_read(0, block.data);
 
-	disk_read(0,block.data);
-
+	/* Print superblock info */
 	printf("superblock:\n");
-	printf("    %d blocks\n",block.super.nblocks);
-	printf("    %d inode blocks\n",block.super.ninodeblocks);
-	printf("    %d inodes\n",block.super.ninodes);
+	if (block.super.magic == FS_MAGIC) {
+		printf("    magic number is valid\n");
+	} else {
+		printf("    magic number is invalid\n");
+	}
+	printf("    %d blocks on disk\n",block.super.nblocks);
+	printf("    %d blocks for inodes\n",block.super.ninodeblocks);
+	printf("    %d inodes total\n",block.super.ninodes);
+
+	/* Iterate through each block containing inodes */
+	for (int i = 0; i < block.super.ninodeblocks; i++) {
+		disk_read(i + 1, block.data);
+
+		/* Iterate through each inode in block */
+		for (int j = 0; j < INODES_PER_BLOCK; j++) {
+			int inode_no = 128 * i + j;
+			struct fs_inode inode = block.inode[j];
+			if (inode.isvalid) {
+				printf("inode %d:\n", inode_no);
+				printf("    size %d bytes\n", inode.size);
+
+				/* Iterate over direct blocks for inode */
+				printf("    direct blocks: ");
+				for (int i = 0; i < POINTERS_PER_INODE; i++) {
+					if (inode.direct[i]) {
+						printf("%d ", inode.direct[i]);
+					}
+				}
+				printf("\n");
+
+				/* Iterate over indirect data blocks for inode */
+				if (inode.indirect) {
+					printf("    indirect block: %d\n", inode.indirect);
+					disk_read(inode.indirect, block.data);
+					printf("    indirect data blocks: ");
+					for (int i = 0; i < POINTERS_PER_BLOCK; i++) {
+						if (block.pointers[i]) {
+							printf("%d ", block.pointers[i]);
+						}
+					}
+					printf("\n");
+				}
+			}
+		}
+	}
 }
 
-int fs_mount()
-{
+int fs_mount() {
 	return 0;
 }
 
-int fs_create()
-{
+int fs_create() {
 	return 0;
 }
 
-int fs_delete( int inumber )
-{
+int fs_delete( int inumber ) {
 	return 0;
 }
 
-int fs_getsize( int inumber )
-{
+int fs_getsize( int inumber ) {
 	return -1;
 }
 
-int fs_read( int inumber, char *data, int length, int offset )
-{
+int fs_read( int inumber, char *data, int length, int offset ) {
 	return 0;
 }
 
-int fs_write( int inumber, const char *data, int length, int offset )
-{
+int fs_write( int inumber, const char *data, int length, int offset ) {
 	return 0;
 }
